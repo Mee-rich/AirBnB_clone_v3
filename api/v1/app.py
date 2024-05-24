a469654681f4ea03531044e83880178101208207
mee-rich <jamessuyi@gmail.com>
2024-05-25 00:56:22 +0100

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,41 @@
 #!/usr/bin/python3
-
+''' Flask Application'''
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+fro os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
+
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*":  {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_db(error):
     '''Close Storage'''
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    ''' 404 error
+    ---
+    responses:
+        404:
+            description: a resource was not found
+    '''
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+        'title': 'AirBnB clone Restful API',
+        'universion': 3
+}
+
+Swagger(app)
 
 if __name__ == "__main__":
     '''Man Function'''
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,4 +5,14 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     '''Status of API'''
-    return jsonify({"status": "OK"})
+    return jsonify({"status": "OK"}
+            )
+
+@app_viwews.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    '''Retrieves the number of each objects by type'''
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {name:storage.count(cls) for name, cls in zip(names, classes)}
+    return jsonify(num_objs)
